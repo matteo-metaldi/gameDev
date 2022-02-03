921dec87be3e9b616ebafd53b931769b35ec5b96
AndreaBForce <andreariccardi911@gmail.com>
2022-02-03 10:19:43 +0100

    hotfix testo

--- Assets/Scripts/CountObjects.cs
+++ Assets/Scripts/CountObjects.cs
@@ -2,26 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class CountObjects : MonoBehaviour
 {
     public string nextLevel;
     public GameObject objToDestry;
-    GameObject objUI;
+    [SerializeField] TextMeshProUGUI m_Object;
     // Start is called before the first frame update
     void Start()
     {
-        objUI = GameObject.Find("ObjectNum");
     }
 
     // Update is called once per frame
     void Update()
     {
-        objUI.GetComponent<Text>().text = ObjectsToCollect.objects.ToString();
-        if(ObjectsToCollect.objects == 0)
+       
+        m_Object.text = ObjectsToCollect.objects.ToString();
+        if (ObjectsToCollect.objects == 0)
         {
             Destroy(objToDestry);
-            objUI.GetComponent<Text>().text = "The door is open!";
+            m_Object.text = "The door is open!";
         }
     }
 }
